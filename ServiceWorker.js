b0bc8a9ebb161a2b583695bf5c4c1083e30d7ee8
An-try <andrey.anderey@gmail.com>
2026-02-08 20:53:16 +0200

    add build

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Over Games-Wedding Invitation-0.0.1";
+const contentToCache = [
+    "Build/WeddingInvitation.loader.js",
+    "Build/WeddingInvitation.framework.js.br",
+    "Build/WeddingInvitation.data.br",
+    "Build/WeddingInvitation.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
